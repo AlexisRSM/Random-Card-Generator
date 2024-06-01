34f0700574abe0f933383db4d2117832eb3a1954
Lex M. <alexismendes98@hotmail.com>
2024-06-01 02:01:24 +0000

    v0.1

---
+++ app.js
@@ -0,0 +1,14 @@
+function randomizer(){
+    let suits = {
+
+    }
+}
+
+
+
+
+
+setInterval(()=>{
+    document.querySelector("row-top").innerText= Math.floor(Math.random() * 9) + 2; //To generate a number between 2 to 10
+    document.querySelector("row-top").innerText=  //To generate a number between 2 to 10
+},500)
